8745959ab8f1dc1019bf816625eccdbf82f77208
Shivani-Gawade <shivanigawade490@gmail.com>
2026-04-23 11:22:59 +0530

    fixed get method

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ const frontendPath = path.join(__dirname, "../client/dist");
 
 app.use(express.static(frontendPath));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
